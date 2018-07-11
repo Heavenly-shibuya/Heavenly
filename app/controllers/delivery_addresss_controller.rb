478a9511003ed9c7bcfb4c51a8310fcd2230cb90
tomokiyao <tomoki9226@gmail.com>
2018-07-11 06:10:49 +0000

    add all

---
+++ app/controllers/delivery_addresss_controller.rb
@@ -0,0 +1,4 @@
+class DeliveryAddresssController < ApplicationController
+  def new
+  end
+end
---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,16 @@
+class ItemsController < ApplicationController
+  def show
+  end
+
+  def edit
+  end
+
+  def new
+  end
+
+  def stock
+  end
+
+  def stock_edit
+  end
+end
