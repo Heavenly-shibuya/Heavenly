06d5ab6392a632a581818eca893eed835a18a86c
Naoto28 <h.naoto.naoto@gmail.com>
2018-07-14 16:42:47 +0000

    stock

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -7,4 +7,7 @@ class ArtistsController < ApplicationController
 
   def edit
   end
+
+  def new
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
   get 'users/index'
   get 'users/show'
   get 'users/edit'
+  get 'items/stock'
+  get 'items/stock_edit'
+
     devise_for :admins, controllers: {
     sessions:      'admins/sessions',
     passwords:     'admins/passwords',
@@ -20,7 +23,6 @@ Rails.application.routes.draw do
   resources :artists
   resources :news_posts
   resources :items
-
   resources :order_items
   resources :favorites, :only => [:index]
   resources :delivery_addresss
