567a5864c5090b7df0f76982c1fc395a0805c7e6
daisuke karino <kingultima1122@gmail.com>
2018-07-17 15:27:00 +0900

    first commit

--- app/controllers/admin/artists_controller.rb
+++ app/controllers/admin/artists_controller.rb
@@ -7,4 +7,19 @@ class Admin::ArtistsController < ApplicationController
 
   def edit
   end
+
+  def new
+   @artist = Artist.new
+  end
+
+  def create
+    artist = @artist.new(artist_params)
+    artist.save
+    redirect_to edit_admin_artist_path(artsit.id)
+  end
+
+private
+    def artist_params
+      params.require(:artist).permit(:top_image, :artist_image, :body, :name)
+    end
 end
