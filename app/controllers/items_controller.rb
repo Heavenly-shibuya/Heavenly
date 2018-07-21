e7a04f1c55751a24f9495de0e538445dbe68c98b
daisuke karino <kingultima1122@gmail.com>
2018-07-21 18:09:09 +0900

    item編集ページ作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,5 +2,18 @@ class ItemsController < ApplicationController
   def show
   end
 
+  def update
+  	@item =Item.find(params[:id])
+  	@item.update(item_params)
+  	redirect_to admin_item_path(@item.id)
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:title, :label, :genre_id, :artist_id, :price, :stock, :item_image,
+     discs_attributes: [:id, :disc_name, :item_id, :_destroy,
+       songs_attributes:[:id, :title, :track_order, :disc_id, :time, :_destroy]])
+  end
 
 end
