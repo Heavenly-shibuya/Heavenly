ed2ff353bb3f8822e150a5c33dd2ae4c04298f1f
tomokiyao <tomoki9226@gmail.com>
2018-07-23 09:48:49 +0000

    検索機能追加

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -39,7 +39,7 @@ class Admin::ItemsController < ApplicationController
   def create
     @item = Item.new(item_params)
     @item.save
-    redirect_to  admin_item_path(@item.id)
+    redirect_to admin_item_path(@item.id)
   end
 
   def stock
@@ -52,7 +52,6 @@ class Admin::ItemsController < ApplicationController
 
   def item_params
     params.require(:item).permit(:title, :label, :genre_id, :artist_id, :price, :stock, :item_image,
-     discs_attributes: [:id, :disc_name, :item_id, :_destroy,
-    　songs_attributes:[:id, :title, :track_order, :disc_id, :time, :_destroy]])
+     discs_attributes: [:id, :disc_name, :item_id, :_destroy, songs_attributes: [:id, :title, :track_order, :disc_id, :time, :_destroy]])
   end
 end
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,13 @@ class ItemsController < ApplicationController
   def show
   end
 
-  
+  def search
+  	@items = Item.page(params[:page]).reverse_order.search(params[:search])
+  	@artists = Artist.page(params[:page]).reverse_order.search(params[:search])
+  	@songs = Song.page(params[:page]).reverse_order.search(params[:search])
+  end
+
+
   def update
   	@item =Item.find(params[:id])
   	@item.update(item_params)
@@ -15,12 +21,6 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:title, :label, :genre_id, :artist_id, :price, :stock, :item_image,
      discs_attributes: [:id, :disc_name, :item_id, :_destroy,
        songs_attributes:[:id, :title, :track_order, :disc_id, :time, :_destroy]])
-  end
-  
-  def search
-  	@items = Item.page(params[:page]).reverse_order.search(params[:search])
-  	@artists = Artist.page(params[:page]).reverse_order.search(params[:search])
-  	@songs = Song.page(params[:page]).reverse_order.search(params[:search])
   end
 
 end
