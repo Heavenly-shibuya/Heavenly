d74ed46f00a8785916ce1d532affb4d0c60eeb78
tomokiyao <tomoki9226@gmail.com>
2018-07-26 10:18:34 +0000

    レビュー機能完成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_25_070451) do
+ActiveRecord::Schema.define(version: 2018_07_26_100003) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -23,9 +23,9 @@ ActiveRecord::Schema.define(version: 2018_07_25_070451) do
     t.datetime "last_sign_in_at"
     t.string "current_sign_in_ip"
     t.string "last_sign_in_ip"
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "name"
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
   end
@@ -143,6 +143,7 @@ ActiveRecord::Schema.define(version: 2018_07_25_070451) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "star"
   end
 
   create_table "rooms", force: :cascade do |t|
@@ -170,8 +171,6 @@ ActiveRecord::Schema.define(version: 2018_07_25_070451) do
     t.datetime "last_sign_in_at"
     t.string "current_sign_in_ip"
     t.string "last_sign_in_ip"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.string "name"
     t.string "last_name"
     t.string "last_name_furi"
@@ -182,6 +181,8 @@ ActiveRecord::Schema.define(version: 2018_07_25_070451) do
     t.string "zip"
     t.string "address"
     t.string "tel"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.datetime "deleted_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
