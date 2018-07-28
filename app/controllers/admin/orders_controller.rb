10bae764cab2189f8c419bf38b28dd2803801d68
daisuke karino <kingultima1122@gmail.com>
2018-07-28 14:45:12 +0900

    order管理者編集中

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,23 +1,59 @@
 class Admin::OrdersController < ApplicationController
 
-  before_action :set_order, only: [:show, :edit, :update, :destroy]
+  before_action :set_order, only: [:show, :edit　:destroy]
   before_action :correct_user
   before_action :authenticate_admin!
 
   def index
-    @orders = Order.all
-  end
-
-  def show
+    @orders = Order.page(params[:page]).reverse.order.per(10)
+    @user = current_user
   end
 
   def edit
   end
 
   def update
+    @order.update(order_params)
+    flash[:notice] = "OrderStatus was successfully Updated."
+    redirect_to @order
+  end
+
+  def show
+  end
+
+  def new
+    @cart = current_cart
+
+    if @cart.cart_items.empty?
+      flash[:notice] = "Cart is empty"
+      redirect_to items_path
+      return
+    end
+
+    @order = Order.new
+    @user = current_user
+
+    @derively = DeliveryAddress.where(user_id: @user.id)
+
+  end
+
+  def create
+    @order = Order.new(order_params)
+    @order.user_id = current_user.id
+    @order.add_items(current_cart)
+
+    if @order.save
+      Cart.destroy(session[:cart_id])
+      session[:cart_id] = nil
+      redirect_to orders_path
+
+    end
   end
 
   def destroy
+    @order.destroy
+    flash[:notice] = "Order was successfully destroyed."
+    redirect_to orders_path
   end
 
   private
@@ -27,14 +63,13 @@ class Admin::OrdersController < ApplicationController
   end
 
   def order_params
-    params.require(:order).permit(:payment, :last_name, :last_name_furi, :first_name, :first_name_furi, :zip, :address, :tel, :user_id )
+    params.require(:order).permit(:payment, :last_name, :last_name_furi, :first_name, :first_name_furi, :zip, :address, :tel, :user_id, :status, :order_delivery )
   end
-end
-  private
+
   def correct_user
     if user_signed_in?
-       redirect_to genres_path
-    end
-  end
+     redirect_to genres_path
+   end
+ end
 
 end
