3ec43849a5413968dce2e0fccf0928f1a393b3c5
daisuke karino <kingultima1122@gmail.com>
2018-07-29 15:23:50 +0900

    order管理者編集中

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,69 +1,32 @@
 class Admin::OrdersController < ApplicationController
 
-  before_action :set_order, only: [:show, :edit　:destroy]
   before_action :correct_user
   before_action :authenticate_admin!
 
   def index
-    @orders = Order.page(params[:page]).reverse.order.per(10)
-    @user = current_user
+    @orders = Order.page(params[:page]).per(10)
+    @user = current_admin
   end
 
   def edit
-  end
-
-  def update
-    @order.update(order_params)
-    flash[:notice] = "OrderStatus was successfully Updated."
-    redirect_to @order
+    @order = Order.find(params[:id])
   end
 
   def show
-  end
-
-  def new
-    @cart = current_cart
-
-    if @cart.cart_items.empty?
-      flash[:notice] = "Cart is empty"
-      redirect_to items_path
-      return
-    end
-
-    @order = Order.new
-    @user = current_user
-
-    @derively = DeliveryAddress.where(user_id: @user.id)
-
-  end
-
-  def create
-    @order = Order.new(order_params)
-    @order.user_id = current_user.id
-    @order.add_items(current_cart)
-
-    if @order.save
-      Cart.destroy(session[:cart_id])
-      session[:cart_id] = nil
-      redirect_to orders_path
-
-    end
+    @order = Order.find(params[:id])
   end
 
   def destroy
+    @order = Order.find(params[:id])
     @order.destroy
     flash[:notice] = "Order was successfully destroyed."
-    redirect_to orders_path
+    redirect_to admin_orders_path
   end
 
   private
 
-  def set_order
-    @order = Order.find(params[:id])
-  end
-
   def order_params
-    params.require(:order).permit(:payment, :last_name, :last_name_furi, :first_name, :first_name_furi, :zip, :address, :tel, :user_id, :status, :order_delivery )
+    params.require(:order).permit(:payment, :last_name, :first_name, :zip, :address, :tel, :user_id, :status, :order_delivery )
   end
 
   def correct_user
