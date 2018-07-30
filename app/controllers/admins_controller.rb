53b1d3fa67714360de12dd8fdc4a779d3c75efa1
daisuke karino <kingultima1122@gmail.com>
2018-07-30 19:51:43 +0900

    修正

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,9 +1,19 @@
 class AdminsController < ApplicationController
+  before_action :correct_user
+  before_action :authenticate_admin!
+
   def index
   	@user = User.all
 
   end
 
   def edit
+  end
+
+  private
+  def correct_user
+    if user_signed_in?
+       redirect_to genres_path
+    end
   end
 end
